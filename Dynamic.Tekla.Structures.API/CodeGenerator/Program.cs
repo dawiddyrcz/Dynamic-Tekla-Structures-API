ded12479feb191391a3e8a791a14b1eb6265772e
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-03-12 19:19:34 +0100

    Created two projects and created Main method in CodeGenerator project

---
+++ Dynamic.Tekla.Structures.API/CodeGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project directory: " + GetProjectDirectory());
+
+            Console.WriteLine("Do you want to generate API codes?\ny - yes\nn - no");
+            var input = Console.ReadLine();
+
+            if (input.Equals("y", StringComparison.InvariantCulture))
+            {
+                GenerateAPICode();
+            }
+        }
+
+        private static string GetProjectDirectory()
+        {
+            var debugFolder = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName;
+            var binFolder = Directory.GetParent(debugFolder);
+            var projectFolder = Directory.GetParent(binFolder.FullName);
+            return projectFolder.FullName;
+        }
+
+        private static void GenerateAPICode()
+        {
+            Console.WriteLine("Generate API code");
+        }
+
+        private static Assembly LoadTeklaStructuresModel()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
---
+++ Dynamic.Tekla.Structures.API/Dynamic.Tekla.Structures.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic.Tekla.Structures.Model
+{
+    public class Class1
+    {
+    }
+}

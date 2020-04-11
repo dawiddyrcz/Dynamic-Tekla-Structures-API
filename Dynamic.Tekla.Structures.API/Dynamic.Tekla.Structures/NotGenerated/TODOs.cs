d32762c01c702cbe8564478045bc6cdab48faa98
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-04-11 22:59:26 +0200

    Added some todos after test

---
+++ Dynamic.Tekla.Structures.API/Dynamic.Tekla.Structures/NotGenerated/TODOs.cs
@@ -0,0 +1,15 @@
+﻿/*
+*Copyright (C) Dawid Dyrcz 2020 - All rights reserved
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic.Tekla.Structures.NotGenerated
+{
+    //TODO new CatalogHandler() rzuca wyjątek że go nie ma
+    //TODO parametery params w Macrobuilder.ListSelect  i TableSelect
+}

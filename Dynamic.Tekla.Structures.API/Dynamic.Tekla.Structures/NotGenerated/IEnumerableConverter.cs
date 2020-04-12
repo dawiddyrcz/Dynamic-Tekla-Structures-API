7c2c669de79ff44658ef5aac1511dc0f938b8a0c
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-04-12 19:30:34 +0200

    Changes in IEnumerableConverter

--- Dynamic.Tekla.Structures.API/Dynamic.Tekla.Structures/NotGenerated/IEnumerableConverter.cs
+++ Dynamic.Tekla.Structures.API/Dynamic.Tekla.Structures/NotGenerated/IEnumerableConverter.cs
@@ -5,23 +5,54 @@
 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * For more details see GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 */
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Dynamic.Tekla.Structures
 {
     internal static class IEnumerableConverter
     {
-        //TODO list<dynamic>
-        public static List<T> ToTSObjects<T>(IEnumerable<T> dynAPIObjects)
+        //TODO test
+        public static List<dynamic> ToTSObjects(IEnumerable dynAPIObjects)
         {
             var enumerator = dynAPIObjects.GetEnumerator();
-            var output = new List<T>();
+            var output = new List<dynamic>();
 
             foreach (dynamic dynObject in dynAPIObjects)
             {
-                output.Add(dynObject.teklaObject);
+                if (dynObject.GetType().ToString().StartsWith("Dynamic.Tekla.Structures."))
+                    output.Add(dynObject.teklaObject);
+                else if (typeof(IEnumerable).IsAssignableFrom(dynObject.GetType()))
+                    output.Add(ToTSObjects(dynObject));
+                else
+                    output.Add(dynObject);
+
             }
 
+            return output;
+        }
+
+        public static List<T> FromTSObjects<T>(dynamic tsObjects)
+        {
+            var output = new List<T>();            
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            foreach (var tsObject in tsObjects)
+            {
+                if (tsObject.GetType().ToString().StartsWith("Tekla.Structures."))
+                {
+                    string converterName = "Dynamic." + tsObject.GetType().ToString() + "_";
+                    var converterType = assembly.GetType(converterName);
+                    var parameters = new object[] { tsObject };
+                    var fromTSObjectMethod = TSActivator.GetMethod("FromTSObject", parameters, converterType);
+
+                    output.Add((T)fromTSObjectMethod.Invoke(null, parameters));
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(tsObject.GetType()))
+                    output.Add(FromTSObjects(tsObject));
+                else
+                    output.Add((T)tsObject);
+            }
             return output;
         }
     }

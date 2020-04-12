623c0471219e3a0db690f426be365a189a1dfc9a
dawiddyrcz <dawid.dyrcz92@gmail.com>
2020-04-12 11:31:22 +0200

    Created class SinglePropertyGenerator and moved code from Class generator

---
+++ Dynamic.Tekla.Structures.API/CodeGenerator/SinglePropertyGenerator.cs
@@ -0,0 +1,218 @@
+﻿/*
+*Copyright (C) Dawid Dyrcz 2020 - All rights reserved
+*/
+
+using Dynamic.Tekla.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerator
+{
+    public static class SinglePropertyGenerator
+    {
+        public static string GetCode(MemberInfo propertyOrField)
+        {
+            Type currentType = null;
+
+            var hasGet = false;
+            var hasSet = false;
+            var isStatic = false;
+
+            if (propertyOrField is PropertyInfo pi)
+            {
+                currentType = pi.PropertyType;
+                hasGet = pi.GetMethod != null;
+                hasSet = pi.SetMethod != null;
+                isStatic = pi.GetMethod.IsStatic;
+            }
+            else if (propertyOrField is FieldInfo fi)
+            {
+                currentType = fi.FieldType;
+                hasGet = true;
+                hasSet = !(fi.IsInitOnly || fi.IsLiteral);
+                isStatic = fi.IsStatic;
+            }
+
+            if (isStatic)
+            {
+                return GenerateStatic_FieldOrProperty(propertyOrField, currentType, hasGet, hasSet);
+            }
+            else
+            {
+                return GenerateNonStatic_FieldOrProperty(propertyOrField, currentType, hasGet, hasSet);
+            }
+        }
+
+        private static string GenerateStatic_FieldOrProperty(MemberInfo propertyOrField, Type currentType, bool hasGet, bool hasSet)
+        {
+            var sb = new StringBuilder();
+            if (IsTeklaType(currentType))
+            {
+                sb.Append("\t\tpublic static ");
+                sb.Append(GetTypeFullName(currentType));
+                sb.Append(" ");
+                sb.Append(propertyOrField.Name);
+
+                if (hasGet)
+                {
+                    sb.Append("\n\t\t{" +
+                    "\n\t\t\tget => " + CorrectIfArray(GetTypeFullName(currentType)) +
+                    "_.FromTSObject(TSActivator.Get_StaticPropertyOrFieldValue(\"$typeFullName\",\""
+                     + propertyOrField.Name + "\"));\n");
+                }
+                if (hasSet)
+                {
+                    sb.Append("\t\t\tset {  TSActivator.Set_StaticPropertyOrFieldValue(\"$typeFullName\",\"" + propertyOrField.Name + "\"," +
+                        CorrectIfArray(GetTypeFullName(currentType)) +
+                        "_.GetTSObject(value)); }");
+                }
+                sb.Append("\n\t\t}\n\n");
+            }
+            else
+            {
+                sb.Append("\t\tpublic static ");
+                sb.Append(GetTypeFullName(currentType));
+                sb.Append(" ");
+                sb.Append(propertyOrField.Name);
+
+                if (hasGet)
+                {
+                    sb.Append("\n\t\t{" + "\n\t\t\tget => (" + GetTypeFullName(currentType) +
+                        ") TSActivator.Get_StaticPropertyOrFieldValue(\"$typeFullName\",\"" + propertyOrField.Name + "\");\n");
+                }
+                if (hasSet)
+                {
+                    sb.Append("\t\t\tset { TSActivator.Set_StaticPropertyOrFieldValue(\"$typeFullName\",\"" + propertyOrField.Name + "\", value); }");
+                }
+                sb.Append("\n\t\t}\n\n");
+            }
+            return sb.ToString();
+        }
+
+        private static string GenerateNonStatic_FieldOrProperty(MemberInfo propertyOrField, Type propertyOrFieldType, bool hasGet, bool hasSet)
+        {
+            var sb = new StringBuilder();
+
+            if (IsTeklaType(propertyOrFieldType))
+            {
+                sb.Append("\t\tpublic ");
+                sb.Append(GetTypeFullName(propertyOrFieldType));
+                sb.Append(" ");
+                sb.Append(propertyOrField.Name);
+
+                if (hasGet)
+                {
+                    sb.Append("\n\t\t{");
+                    sb.Append("\n\t\t\tget\n\t\t\t{\n");
+                    sb.Append("\t\t\t\ttry {\n");
+                    sb.Append("\t\t\t\treturn ");
+                    sb.Append(CorrectIfArray(GetTypeFullName(propertyOrFieldType)));
+                    sb.Append("_.FromTSObject($dfield.");
+                    sb.Append(propertyOrField.Name);
+                    sb.Append(");\n");
+                    sb.Append("\t\t\t\t} catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)\n");
+                    sb.Append("\t\t\t\t { throw DynamicAPINotFoundException.CouldNotFindProperty(\"" + propertyOrField.Name + "\", ex); }\n");
+                    sb.Append("\t\t\t}\n");
+                }
+                if (hasSet)
+                {
+                    sb.Append("\t\t\tset\n\t\t\t{\n");
+                    sb.Append("\t\t\t\ttry {\n");
+                    sb.Append("\t\t\t\t$dfield.");
+                    sb.Append(propertyOrField.Name);
+                    sb.Append(" = ");
+                    sb.Append(CorrectIfArray(GetTypeFullName(propertyOrFieldType)));
+                    sb.Append("_.GetTSObject(value);\n");
+                    sb.Append("\t\t\t\t} catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)\n");
+                    sb.Append("\t\t\t\t { throw DynamicAPINotFoundException.CouldNotFindProperty(\"" + propertyOrField.Name + "\", ex); }\n");
+                    sb.Append("\t\t\t}\n");
+                }
+                sb.Append("\t\t}\n\n");
+            }
+            else
+            {
+                sb.Append("\t\tpublic ");
+                sb.Append(GetTypeFullName(propertyOrFieldType));
+                sb.Append(" ");
+                sb.Append(propertyOrField.Name);
+
+                if (hasGet)
+                {
+                    sb.Append("\n\t\t{\n");
+                    sb.Append("\t\t\tget\n\t\t\t{\n");
+                    sb.Append("\t\t\t\ttry {\n");
+
+                    if (propertyOrFieldType.Equals(typeof(System.Collections.ArrayList)))
+                    {
+                        sb.Append("\t\t\t\t\treturn TSActivator.ConvertArrayList($dfield.");
+                        sb.Append(propertyOrField.Name);
+                        sb.Append(")");
+                    }
+                    else
+                    {
+                        sb.Append("\t\t\t\t\treturn $dfield.");
+                        sb.Append(propertyOrField.Name);
+                    }
+
+                    sb.Append(";\n");
+                    sb.Append("\t\t\t\t} catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)\n");
+                    sb.Append("\t\t\t\t { throw DynamicAPINotFoundException.CouldNotFindProperty(\"" + propertyOrField.Name + "\", ex); }\n");
+                    sb.Append("\t\t\t}\n");
+                }
+                if (hasSet)
+                {
+                    sb.Append("\t\t\tset\n\t\t\t{\n");
+                    sb.Append("\t\t\t\ttry {\n");
+                    sb.Append("\t\t\t\t\t$dfield.");
+                    sb.Append(propertyOrField.Name);
+                    sb.Append(" = ");
+
+                    if (propertyOrFieldType.Equals(typeof(System.Collections.ArrayList)))
+                    {
+                        sb.Append("TSActivator.ConvertToTSArrayList(value);");
+                    }
+                    else
+                    {
+                        sb.Append("value;");
+                    }
+
+                    sb.Append("\n");
+                    sb.Append("\t\t\t\t} catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)\n");
+                    sb.Append("\t\t\t\t { throw DynamicAPINotFoundException.CouldNotFindProperty(\"" + propertyOrField.Name + "\", ex); }\n");
+                    sb.Append("\t\t\t}");
+                }
+                sb.Append("\n\t\t}\n\n");
+            }
+            return sb.ToString();
+        }
+
+        private static string CorrectIfArray(string dynamicTypeFullName)
+        {
+            if (dynamicTypeFullName.StartsWith("Dynamic."))
+            {
+                if (dynamicTypeFullName.EndsWith("[]"))
+                {
+                    return dynamicTypeFullName.Substring(0, dynamicTypeFullName.Length - 2) + "Array";
+                }
+                else return dynamicTypeFullName;
+            }
+            else return dynamicTypeFullName;
+        }
+
+        private static string GetTypeFullName(Type type)
+        {
+            return TypeFullName.GetTypeFullName_WithDynamic(type);
+        }
+
+
+        private static bool IsTeklaType(Type type)
+        {
+            return type?.FullName?.StartsWith("Tekla.Structures") ?? false;
+        }
+
+    }
+}
